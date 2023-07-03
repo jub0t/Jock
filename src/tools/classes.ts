3b7be120c886e194ce83f397bcb92806dbf64ed8
Jareer Abdullah <jforeverything2007@gmail.com>
2023-07-03 15:22:05 +0200

    Update classes.ts

--- src/tools/classes.ts
+++ src/tools/classes.ts
@@ -3,17 +3,18 @@ import { Export } from "../parser";
 
 export function getAllClassChildren<T extends { new(...args: any[]): any }>(target_class: T): Export[] {
     let children: Export[] = [];
-    const proto: any = target_class.prototype;
+    const proto = target_class.prototype;
 
     if (!proto) {
         return children;
     }
 
-    const descriptors = Object.getOwnPropertyDescriptors(proto)
+    const methods = Object.getOwnPropertyDescriptors(proto)
+    const classes = getInnerClasses(target_class)
 
     // Process Children
-    Object.keys(descriptors).forEach((name: any) => {
-        const method = descriptors[name]
+    Object.keys(methods).forEach((name: any) => {
+        const method = methods[name]
         const value = method.value
         const ctype: InstanceType = GetType(value);
         const is_basic: boolean = IsBasic(ctype);
@@ -29,11 +30,28 @@ export function getAllClassChildren<T extends { new(...args: any[]): any }>(targ
         children.push(child)
     });
 
+    classes.forEach(innerClass => {
+        let classChildren = getAllClassChildren(innerClass)
+        children.push({
+            Key: innerClass.name,
+            Value: innerClass,
+            IsBasic: false,
+            Type: InstanceType.Class,
+            Children: classChildren,
+        } as Export)
+    });
+
     return children;
 }
 
 export function removeDefaults(target_class: string[]) {
     return target_class.filter(i => {
         return !["length", "name", "prototype"].includes(i)
+    })
+}
+
+export function getInnerClasses<T>(parent_class: T) {
+    return removeDefaults(Object.getOwnPropertyNames(parent_class)).map((name) => {
+        return (parent_class as any)[name]
     })
 }
