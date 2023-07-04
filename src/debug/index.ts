7488ceddb1c89620484dbfca59883e9dc96fb7af
Jareer Abdullah <jforeverything2007@gmail.com>
2023-07-04 21:47:54 +0200

    Update

--- src/debug/index.ts
+++ src/debug/index.ts
@@ -1,11 +1,14 @@
+import { LocalGenerator } from "../generator";
 import { Export } from "../parser";
 import { InstanceType } from "../tools/types";
 
 class Debug {
     PrintStructure<T>(parent: Export<T>, depth = 0) {
+        const dir = LocalGenerator.exportToDirectory(parent);
+
         if (parent.Children.length > 0) {
             for (const child of parent.Children) {
-                console.log(`${" ".repeat(depth * 4)}- ${parent.Key}.${child.Key}`);
+                console.log(`${" ".repeat(depth * 4)}[${dir.Id}] - ${parent.Key}.${child.Key}`);
 
                 if (child.Type == InstanceType.Class) {
                     // We don't proccess the constructor because child.constructor = Parent
@@ -16,7 +19,7 @@ class Debug {
             }
         }
         else {
-            console.log(`${" ".repeat(depth * 4)}- ${parent.Key}<${parent.Type}>(${parent.FunctionData != null ? parent.FunctionData.Params.join(", ") : ""})`);
+            console.log(`${" ".repeat(depth * 4)}[${dir.Id}] - ${parent.Key}<${parent.Type}>(${parent.FunctionData != null ? parent.FunctionData.Params.join(", ") : ""})`);
         }
     }
 }
--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,32 +1,67 @@
 import { Export } from "../parser"
+import { InstanceType, IsBasic } from "../tools/types"
+import { randID } from "../tools/uuid"
 
-export interface Page { }
-export interface DirFileConfig {
-    Icon: string
-    Title: string
+export interface Directory<T> {
+    Id: String,
+    Name: String
+    Icon: String
+    RawCode: String
+    IsFolder: Boolean
+    Children?: Directory<T>[]
 }
 
-class Generator {
-    #pages = []
-
-    generateDirFile(dc: DirFileConfig) {
-        let data = []
-
-        if (dc.Icon != null) {
-            data.push(`icon: ${dc.Icon}`)
-        }
-
-        if (dc.Title != null) {
-            data.push(`title: ${dc.Title}`)
-        }
-
-        return data.join("\n")
+class Generator<T> {
+    #pages: { [key: string]: Directory<T>[] } = {
+        globals: [],
+        functions: [],
+        classes: [],
+        objects: [],
     }
 
+    getIconByType(typen: InstanceType): String {
+        if (IsBasic(typen)) {
+            return "heroicons-outline:globe-alt"
+        } else if (typen === InstanceType.Object) {
+            return "heroicons-outline:cube-transparent"
+        } else if (typen === InstanceType.Class) {
+            return "heroicons-outline:document-text"
+        } else if (typen === InstanceType.Function) {
+            return "heroicons-outline:beaker"
+        } else {
+            return ""
+        }
+    }
 
-    generateCore<T>(exp: Export<T>) {
+    generateCode<T>(dir: Directory<T>): String {
+        return ""
+    }
 
+    exportToDirectory<T>(exp: Export<T>): Directory<T> {
+        const dir: Directory<T> = {
+            Id: randID(16),
+            Icon: this.getIconByType(exp.Type),
+            IsFolder: exp.Children.length > 0,
+            Name: exp.Key,
+            Children: [],
+            RawCode: ""
+        };
+
+        dir.RawCode = this.generateCode(dir);
+
+        if (exp.Type == InstanceType.Class) {
+            exp.Children.forEach(child => {
+                if (child.Key != "constructor") {
+                    const childDir = this.exportToDirectory(child)
+                    dir.Children?.push(childDir)
+                }
+            });
+        }
+
+        return dir;
     }
 }
+const LocalGenerator = new Generator()
 
+export { LocalGenerator }
 export default Generator
