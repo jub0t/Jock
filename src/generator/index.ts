7a7c6ce8b406b0e71c4d3616eb7099655e4fd28e
Jareer Abdullah <jforeverything2007@gmail.com>
2023-07-05 13:21:12 +0200

    Update index.ts

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -2,14 +2,27 @@ import { Export } from "../parser"
 import { InstanceType, IsBasic } from "../tools/types"
 import { randID } from "../tools/uuid"
 
+enum DirectoryType {
+    Global,
+    Class,
+    Function,
+    Object,
+    Other
+}
+
 export interface Directory<T> {
     Id: String,
     Name: String
     Icon: String
-    RawCode: String
+    RawCode?: String
+    DocCode?: String
     IsFolder: Boolean
+    Type: DirectoryType,
     Children?: Directory<T>[]
 }
+
+
+type PossibleString = String | undefined
 
 class Generator<T> {
     #pages: { [key: string]: Directory<T>[] } = {
@@ -33,17 +46,35 @@ class Generator<T> {
         }
     }
 
+    changeType(itype: InstanceType): DirectoryType {
+        if (IsBasic(itype)) {
+            return DirectoryType.Global
+        } else {
+            if (itype == InstanceType.Class) {
+                return DirectoryType.Class
+            } else if (itype == InstanceType.Function) {
+                return DirectoryType.Function
+            } else if (itype == InstanceType.Object) {
+                return DirectoryType.Object
+            } else {
+                return DirectoryType.Other
+            }
+        }
+    }
+
     exportToDirectory<T>(exp: Export<T>): Directory<T> {
         const dir: Directory<T> = {
             Id: randID(16),
             Icon: this.getIconByType(exp.Type),
             IsFolder: exp.Children.length > 0,
             Name: exp.Key,
+            Type: this.changeType(exp.Type),
             Children: [],
-            RawCode: ""
+            DocCode: ""
         };
 
-        dir.RawCode = this.generateCode(dir);
+        dir.RawCode = exp.ClassData?.RawCode || exp.FunctionData?.RawCode
+        dir.DocCode = this.generateCode(dir);
 
         if (exp.Type == InstanceType.Class) {
             exp.Children.forEach(child => {
@@ -57,8 +88,24 @@ class Generator<T> {
         return dir;
     }
 
-    generateCode<T>(dir: Directory<T>): String {
-        return ""
+    getCodeBlock(lang: string, label: string, code: PossibleString): PossibleString {
+        if (code == null) return;
+
+        let results = "";
+        const codeblock = "```" + lang + ` [${label}]` + "\n" + code + "\n```";
+
+        results += `# ${label}`
+        results += `::code-groupn\n${codeblock}\n::`
+
+        return results
+    }
+
+    generateCode<T>(dir: Directory<T>): PossibleString {
+        if (dir.Type == DirectoryType.Class) {
+            return this.getCodeBlock("js", "Class Code", dir.RawCode)
+        } else {
+            return ""
+        }
     }
 }
 const LocalGenerator = new Generator()
