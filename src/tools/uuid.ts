85969b2d7102d7ba256a41e8305aa7201709f5c8
Jareer Abdullah <jforeverything2007@gmail.com>
2023-07-06 17:13:14 +0200

    Update

--- src/tools/uuid.ts
+++ src/tools/uuid.ts
@@ -1,10 +1,10 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 export function randID(length: number): string {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let id = '';
 
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        id += characters.charAt(randomIndex);
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
     return id;
